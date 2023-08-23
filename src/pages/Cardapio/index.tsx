4cd06f0ffeb7fa6edc5a1d7de033ba3b0db1d978
victoramattosc <victorcarbelotti0306@gmail.com>
2023-08-23 08:55:44 -0300

    Header e Começo section

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -10,6 +10,9 @@ return(
       <header className={styles.header}>
         <div className={styles.header__text}>"A casa do código e da massa"</div>
       </header>
+      <section>
+        <h3>Cardápio</h3>
+      </section>
     </main>
 );
 }
